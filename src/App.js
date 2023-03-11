944bcf8b56c68364bfeea8c938060c3a20ab42c2
nizamuddin15bd <nizamuddin15bd@gmail.com>
2023-03-11 14:16:05 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth } from "firebase/auth";
+import app from './firebase-init';
 
+
+const auth = getAuth(app);
 function App() {
+  const handleEmailChange = event => {
+    console.log(event.target.value);
+  }
+  const handlePasswordChange = event => {
+    console.log(event.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input onChange={handleEmailChange} type="email" name="" id="" />
+        <input onChange={handlePasswordChange} type="password" name="" id="" />
+      </form>
     </div>
   );
 }
---
+++ src/firebase-init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAYhzrc1XSPPH6xTQjKTptggAZz4KP8YO0",
+    authDomain: "email-password-auth-b5c73.firebaseapp.com",
+    projectId: "email-password-auth-b5c73",
+    storageBucket: "email-password-auth-b5c73.appspot.com",
+    messagingSenderId: "476979301553",
+    appId: "1:476979301553:web:13991bb1ce02816eefa690"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
